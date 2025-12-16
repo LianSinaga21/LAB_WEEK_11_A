2511b8d9ec74730e2b057d884645d943b0936280
LianSinaga21 <parsauliansinaga17@gmail.com>
2025-12-16 09:41:48 +0700

    Initial commit

---
+++ app/src/main/java/com/example/lab_week_11_a/preference/PreferenceApplication.kt
@@ -0,0 +1,20 @@
+package com.example.lab_week_11_a.preference
+
+import android.app.Application
+import android.content.Context
+
+class PreferenceApplication : Application() {
+
+    lateinit var preferenceWrapper: PreferenceWrapper
+
+    override fun onCreate() {
+        super.onCreate()
+
+        preferenceWrapper = PreferenceWrapper(
+            getSharedPreferences(
+                "prefs",
+                Context.MODE_PRIVATE
+            )
+        )
+    }
+}
